7a8b189153711deb41a26c484f9d66a02f3c1d84
Luis Rosales <luigirosales84@gmail.com>
2020-07-13 20:10:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let who=['the dog ','my grandma ','his turtle ','my bird '];
+let what=['ate ', 'pissed on ', 'crushed ','broke '];
+let whatever=['the homework ', 'the essay ', 'the thesis', 'the assignment']
+let when=['before the class', 'right in time' , 'when I finished' , 'during my lunch', 'while I was praying'];
+
+var excuse = document.querySelector('#excuse');
+
+const excuse1= who[Math.floor(Math.random() * who.length)];
+const excuse2= what[Math.floor(Math.random() * what.length)];
+const excuse3= whatever[Math.floor(Math.random() * whatever.length)];
+const excuse4= when[Math.floor(Math.random() * when.length)];
+
+excuse.innerHTML=excuse1+excuse2+excuse3+excuse4;
+console.log(excuse);
