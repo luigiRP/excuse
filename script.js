bfdafc8f35513c461ab583d9639510fbf99b27a4
luigiRP <luigirosales84@gmail.com>
2020-07-22 10:17:51 +0000

    yo sumo letras :D

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-let who=['the dog ','my grandma ','his turtle ','my bird '];
-let what=['ate ', 'pissed on ', 'crushed ','broke '];
-let whatever=['the homework ', 'the essay ', 'the thesis', 'the assignment']
+let who=['the dog','my grandma','his turtle','my bird'];
+let what=['ate', 'pissed on', 'crushed','broke'];
+let whatever=['the homework', 'the essay', 'the thesis', 'the assignment']
 let when=['before the class', 'right in time' , 'when I finished' , 'during my lunch', 'while I was praying'];
 
 var excuse = document.querySelector('#excuse');
@@ -11,6 +11,6 @@ const excuse2= what[Math.floor(Math.random() * what.length)];
 const excuse3= whatever[Math.floor(Math.random() * whatever.length)];
 const excuse4= when[Math.floor(Math.random() * when.length)];
 
-excuse.innerHTML=excuse1.concat(excuse2,excuse3,excuse4);
+excuse.innerHTML=excuse1.concat(' ', excuse2,' ', excuse3,' ',excuse4);
 console.log(excuse);
 }
