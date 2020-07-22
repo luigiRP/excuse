5854035b7c6c18fec7b1894a81dc848edc5f0cf3
luigiRP <luigirosales84@gmail.com>
2020-07-22 09:55:30 +0000

    no me gusta usar concat

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.onload = function() {
 let who=['the dog ','my grandma ','his turtle ','my bird '];
 let what=['ate ', 'pissed on ', 'crushed ','broke '];
 let whatever=['the homework ', 'the essay ', 'the thesis', 'the assignment']
@@ -10,5 +11,6 @@ const excuse2= what[Math.floor(Math.random() * what.length)];
 const excuse3= whatever[Math.floor(Math.random() * whatever.length)];
 const excuse4= when[Math.floor(Math.random() * when.length)];
 
-excuse.innerHTML=excuse1+excuse2+excuse3+excuse4;
+excuse.innerHTML=excuse1.concat(excuse2,excuse3,excuse4);
 console.log(excuse);
+}
